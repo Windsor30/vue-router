d782e9665865ac9394a73c7163a47e2c69c2b2ed
Windsor30 <904402987@qq.com>
2018-01-08 21:16:35 +0800

    '1.0.0'

---
+++ vue-router/router.js
@@ -0,0 +1,25 @@
+var router = new VueRouter({
+    routes:[
+        {
+            path:'/',
+            component:Main,
+            children:[
+                {
+                    path:'',
+                    components:{
+                        left:Left,
+                        right:Right,
+                    },
+                },
+            ],
+        },
+        {
+            path:'/quick',
+            component:Quick
+        }
+    ]
+})
+var root=new Vue({
+    el:'#root',
+    router,
+})
